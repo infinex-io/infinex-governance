a9ca90e43a6e1d39b48b353db71a9208df1385e3
Kevin Upton <k_upton@live.com>
2023-10-09 09:03:40 +1100

    completed deployments

--- packages/treasury-council/scripts/initialise.js
+++ packages/treasury-council/scripts/initialise.js
@@ -14,16 +14,14 @@ async function main() {
   );
 
   const OWNER = '0xbb89AAc08376cdBb3fB9F16cd8219C9260e50e98';
-  const CONTRACT_ADDRESS = '0x6Fb042D40d118f74D6D6b3676D82FAc1716431b1';
+  const CONTRACT_ADDRESS = '0xaC29b8c5413044F74Ff80679279987acc4E0559d';
   const DEBT_SHARE_CONTRACT = '0xd02FE0c7fb2FFa71f33Fc1785ec1e1a310EB724D';
-  const SNAPSHOT_DATE = '2023-10-06T08:00:00Z';
-  const NOMINATION_START_DATE = '2023-10-06T08:00:00Z';
-  const NOMINATION_DURATION = hoursToSeconds(1);
-  const VOTING_DURATION = hoursToSeconds(2);
+  const NOMINATION_START_DATE = '2023-10-06T09:45:00Z';
+  const NOMINATION_DURATION = 10 * 60;
+  const VOTING_DURATION = hoursToSeconds(1);
   const MAX_UINT64 = BigInt(2) ** BigInt(64) - BigInt(1);
 
   const schedule = {
-    snapshotId: isoToSeconds(SNAPSHOT_DATE),
     nominationStartDate: isoToSeconds(NOMINATION_START_DATE),
     votingStartDate: NOMINATION_DURATION + isoToSeconds(NOMINATION_START_DATE),
     epochEndDate: VOTING_DURATION + NOMINATION_DURATION + isoToSeconds(NOMINATION_START_DATE),
@@ -96,22 +94,6 @@ async function main() {
   ) {
     tx = await ownerModule.nominateNewOwner(OWNER);
     console.log('nominate owner', tx.hash);
-    await tx.wait();
-  }
-
-  let debtShareSnapshotId;
-  try {
-    debtShareSnapshotId = BigInt(await electionModule.getDebtShareSnapshotId());
-  } catch (e) {
-    debtShareSnapshotId = BigInt(0);
-  }
-
-  if (
-    debtShareSnapshotId !== BigInt(schedule.snapshotId) &&
-    schedule.nominationStartDate * 1000 <= Date.now()
-  ) {
-    tx = await electionModule.setDebtShareSnapshotId(schedule.snapshotId);
-    console.log('set snapshot id', tx.hash);
     await tx.wait();
   }
 
---
+++ packages/treasury-council/scripts/snapshot.js
@@ -0,0 +1,34 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+  const electionModuleFactory = await ethers.getContractFactory(
+    'contracts/modules/ElectionModule.sol:ElectionModule'
+  );
+
+  const CONTRACT_ADDRESS = '0xaC29b8c5413044F74Ff80679279987acc4E0559d';
+
+  const electionModule = electionModuleFactory.attach(CONTRACT_ADDRESS);
+  let tx;
+
+  if ((await electionModule.getCurrentPeriod()).eq(1)) {
+    let debtShareSnapshotId;
+    try {
+      debtShareSnapshotId = BigInt(await electionModule.getDebtShareSnapshotId());
+    } catch (e) {
+      debtShareSnapshotId = BigInt(0);
+    }
+
+    if (debtShareSnapshotId === BigInt(0)) {
+      tx = await electionModule.setDebtShareSnapshotId(await ethers.provider.getBlockNumber());
+      console.log('set snapshot id', tx.hash);
+      await tx.wait();
+      console.log('done');
+    } else {
+      console.log('snapshot already staken');
+    }
+  } else {
+    console.log('invalid time period');
+  }
+}
+
+main().catch((e) => console.error(e));
